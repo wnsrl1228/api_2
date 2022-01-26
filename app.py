529669a19127c60ef8b765bea6f33d6e11d5b22d
=JUN KIM <=wnsrl981228@gmail.com>
2022-01-26 06:28:37 -0800

    init Flask : No database

---
+++ app.py
@@ -0,0 +1,84 @@
+from flask import Flask, jsonify, request
+from flask.json import JSONEncoder
+
+class CustomJSONEncoder(JSONEncoder):
+    def default(self,obj):
+        if isinstance(obj, set):
+            return list(obj)
+        return JSONEncoder.default(self,obj)
+app = Flask(__name__)
+app.users = {}
+app.id_count = 1
+
+app.tweets = []
+
+app.json_encoder = CustomJSONEncoder
+@app.route("/sign-up",methods=['POST'])
+def sign_up():
+    new_user = request.json
+    new_user["id"] = app.id_count
+    app.users[app.id_count] = new_user
+    app.id_count += 1
+
+    return jsonify(new_user)
+@app.route("/tweet",methods=['POST'])
+def tweet():
+    payload = request.json
+    user_id = int(payload["id"])
+    tweet = payload["tweet"]
+
+    if user_id not in app.users:
+        return "NO ID"
+
+    if len(tweet) > 300:
+        return "300 over"
+
+    user_id = int(payload['id'])
+    
+    app.tweets.append({
+        'user_id' : user_id,
+        'tweet' : tweet
+        })
+    return '',200
+
+@app.route("/follow",methods=['POST'])
+def follow():
+    payload = request.json
+    user_id = int(payload['id'])
+    follow_id = int(payload['follow'])
+
+    if user_id not in app.users or follow_id not in app.users:
+        return "Can't find id or follow_id"
+
+    user = app.users[user_id]
+    user.setdefault('follow',set()).add(follow_id)
+
+    return jsonify(user)
+@app.route("/unfollow",methods=['POST'])
+def unfollow():
+    payload = request.json
+    user_id = int(payload['id'])
+    unfollow_id = int(payload['unfollow'])
+
+    if user_id not in app.users or unfollow_id not in app.users:
+        return "Can't find id or unfollow_id"
+    user = app.users[user_id]
+    user.setdefault('follow',set()).discard(unfollow_id)
+
+    return jsonify(user)
+@app.route("/timeline/<int:user_id>",methods=['GET'])
+def timeline(user_id):
+    if user_id not in app.users:
+        return "Can't find id"
+    follow_list = app.users[user_id].get('follow',set())
+    follow_list.add(user_id)
+    timeline = [tweet for tweet in app.tweets if tweet['user_id'] in follow_list]
+
+    return jsonify({
+        'user_id' : user_id,
+        'timeline' : timeline
+        })
+
+@app.route("/ping", methods=['GET'])
+def ping():
+    return "pong"
