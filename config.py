ea9352b4474c86546f65ca3172b82c2bb0556d42
=JUN KIM <=wnsrl981228@gmail.com>
2022-02-08 02:54:33 -0800

    Add Architecture

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ db = {
 
 DB_URL = f"mysql+mysqlconnector://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['database']}?charset=utf8"
 JWT_SECRET_KEY        = 'SOME_SUPER_SECRET_KEY'
+JWT_EXP_DELTA_SECONDS = 7 * 24 * 60 * 60
 
 test_db = {
     'user'     : 'root',
@@ -18,9 +19,8 @@ test_db = {
     'database' : 'test_db'
 }
 
-test_config = {
+test_config = {		
     'DB_URL' : f"mysql+mysqlconnector://{test_db['user']}:{test_db['password']}@{test_db['host']}:{test_db['port']}/{test_db['database']}?charset=utf8",
     'JWT_SECRET_KEY' : 'SOME_SUPER_SECRET_KEY',
     'JWT_EXP_DELTA_SECONDS' : 7 * 24 * 60 * 60
 }
-
